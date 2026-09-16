00c84aad431b84702f4bde2c3f641948ba5edddb
wongyiuming <146617903+wongyiuming@users.noreply.github.com>
2026-09-16 21:00:34 +0800

    feat: start Receiver after boot

---
+++ n1s-receiver/app/src/main/java/com/n1s/receiver/BootReceiver.java
@@ -0,0 +1,19 @@
+package com.n1s.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (!Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) return;
+        Intent service = new Intent(context, ReceiverService.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(service);
+        } else {
+            context.startService(service);
+        }
+    }
+}
